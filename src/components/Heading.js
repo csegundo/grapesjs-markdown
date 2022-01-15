f6fcfe5f63aec07db1e252330e1248fd45bef2ea
csegundo <carlossn20165@gmail.com>
2022-01-15 18:22:57 +0100

    Addes CSS. Allow heading to edit

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -5,10 +5,44 @@ export default (editor, { domc, classPrefix }) =>{
         model: {
             defaults: {
                 name : editor.I18n.t('grapesjs-markdown.components.names.heading'),
-                tagName : 'h1',
-                stylable : true,//[],
-                attributes : { class : classPrefix + 'heading' },
+                tagName : 'div',
+                editable : true,
+                stylable : [], //true,
+                attributes : {
+                    class : classPrefix + 'heading',
+                    'data-gjs-type' : type,
+                    'data-gjs-level' : 1, // default
+                    // 'data-gjs-editable' : true
+                },
+                traits : [
+                    {
+                        type : 'select',
+                        label : editor.I18n.t('grapesjs-markdown.traits.labels.headingLevel'),
+                        name : 'level',
+                        changeProp: true,
+                        options : [
+                            { name : `${editor.I18n.t('grapesjs-markdown.other.level')} 1`, value : 1 },
+                            { name : `${editor.I18n.t('grapesjs-markdown.other.level')} 2`, value : 2 },
+                            { name : `${editor.I18n.t('grapesjs-markdown.other.level')} 3`, value : 3 },
+                            { name : `${editor.I18n.t('grapesjs-markdown.other.level')} 4`, value : 4 },
+                            { name : `${editor.I18n.t('grapesjs-markdown.other.level')} 5`, value : 5 },
+                            { name : `${editor.I18n.t('grapesjs-markdown.other.level')} 6`, value : 6 },
+                        ]
+                    }
+                ]
             },
+            init(){
+                this.listenTo(this, 'change:level', this.changeLevel);
+            },
+            changeLevel(){
+                try{
+                    const newLevel = this.get('traits').where({ name : 'level' })[0].get('value');
+                    this.addAttributes({ 'data-gjs-level' : newLevel });
+                    // reload canvas?
+                } catch(e){
+                    console.error(`Component "${type}" changeLevel() exception`);
+                }
+            }
         },
         view: {
             events : {
--- src/locale/en.js
+++ src/locale/en.js
@@ -2,11 +2,31 @@ export default {
     'grapesjs-markdown': {
         category : 'Markdown',
         classPrefix : 'gjs-md-',
+        /**
+         * Components name
+         */
         components : {
             names : {
                 heading : 'Heading'
             }
         },
+        /**
+         * Component traits attr (model)
+         */
+        traits : {
+            labels : {
+                headingLevel : 'Level type'
+            }
+        },
+        /**
+         * Other stuff
+         */
+        other : {
+            level : 'Level'
+        },
+        /**
+         * Plugin command names
+         */
         commands : {
             names : {
                 export : 'gjs-md-export'
